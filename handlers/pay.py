f50a24b6a8bb0ec1eb08eaf9e49fa059032396fb
Senthil Kumaran <senthil@uthcode.com>
2014-07-06 20:36:08 -0700

    query entity, count and then action

--- handlers/pay.py
+++ handlers/pay.py
@@ -1,21 +1,28 @@
 import webapp2
 from models.person import PersonEntityClass
 
-from utils import pay_template
+from utils import pay_template, form_template, cancel_template
+
 
 class PaypalHandler(webapp2.RequestHandler):
   def get(self, email):
-    person = PersonEntityClass.query(PersonEntityClass.email==email).fetch()[0]
-    values = {
-      'name': person.name,
-      'spouse': person.spouse,
-      'family': person.family,
-      'veg': person.veg,
-      'total_veg_cost': person.total_veg_cost,
-      'nonveg': person.nonveg,
-      'total_non_veg_cost': person.total_non_veg_cost,
-      'total_food_cost': person.total,
-      'email': person.email
-    }
-
-    self.response.write(pay_template.render(values))
+    if email:
+      person_query = PersonEntityClass.query(PersonEntityClass.email==email)
+      if person_query.count():
+        person = person_query.fetch()[0]
+        values = {
+          'name': person.name,
+          'spouse': person.spouse,
+          'family': person.family,
+          'veg': person.veg,
+          'total_veg_cost': person.total_veg_cost,
+          'nonveg': person.nonveg,
+          'total_non_veg_cost': person.total_non_veg_cost,
+          'total_food_cost': person.total,
+          'email': person.email
+        }
+        self.response.write(pay_template.render(values))
+      else:
+        self.response.write(cancel_template.render())
+    else:
+      self.redirect('/')
