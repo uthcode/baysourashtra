babfa9beb654814bfcba59e8086d034a4ef8effb
Senthil Kumaran <senthil@uthcode.com>
2014-07-10 23:14:50 -0700

    list all

---
+++ handlers/listall.py
@@ -0,0 +1,11 @@
+import webapp2
+from models.person import PersonEntityClass
+from utils import list_template
+
+class ListAllHandler(webapp2.RequestHandler):
+  def get(self):
+    person_query = PersonEntityClass.query().fetch()
+    values = {
+      'all': person_query
+    }
+    self.response.write(list_template.render(values))
