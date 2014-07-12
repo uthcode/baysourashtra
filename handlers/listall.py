d04abe92cce716039d93407e5fb47fd44a8db862
Senthil Kumaran <senthil@uthcode.com>
2014-07-11 21:12:21 -0700

    Summary Details

--- handlers/listall.py
+++ handlers/listall.py
@@ -6,6 +6,12 @@ class ListAllHandler(webapp2.RequestHandler):
   def get(self):
     person_query = PersonEntityClass.query().fetch()
     values = {
-      'allrecords': person_query
+      'allrecords': person_query,
+      'total_registered': len(person_query),
+      'total_veg' : sum([p.veg for p in person_query]),
+      'total_nonveg' : sum([p.nonveg for p in person_query]),
+      'total_payment': sum([p.total for p in person_query]),
+      'total_received': sum([p.total for p in person_query if p.paid])
     }
+
     self.response.write(list_template.render(values))
